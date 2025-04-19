8d5eb3365ac0a8493f3e02fb3da4557741e33e70
Daesiker <eotlr680@naver.com>
2025-04-19 16:56:42 +0900

    13주차 파트04

---
+++ book-store/src/pages/Detail.tsx
@@ -0,0 +1,17 @@
+import Footer from "../components/common/Footer";
+import Header from "../components/common/Header";
+import Sidebar from "./Sidebar";
+
+function Detail() {
+    return (
+        <>
+        <Header/>
+        <Sidebar/>
+        <div>디테일</div>
+        <Footer/>
+        </>
+        
+    )
+}
+
+export default Detail;
---
+++ book-store/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import Footer from "../components/common/Footer";
+import Header from "../components/common/Header";
+
+function Home() {
+    return (
+        <>
+        <Header/>
+        <div>home body</div>
+        <Footer/>
+        </>
+    );
+}
+
+export default Home;
