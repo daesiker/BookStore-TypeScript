246c3635d8f6c4fb2ba43299ed109b5012bcd9a3
Daesiker <eotlr680@naver.com>
2025-04-20 16:35:04 +0900

    13주차 파트05

--- book-store/src/pages/Home.tsx
+++ book-store/src/pages/Home.tsx
@@ -1,12 +1,16 @@
-import Footer from "../components/common/Footer";
-import Header from "../components/common/Header";
+
+import Button from "../components/common/Button";
+import InputText from "../components/common/InputText";
+import Title from "../components/common/Title";
 
 function Home() {
     return (
         <>
-        <Header/>
-        <div>home body</div>
-        <Footer/>
+        <Title size="medium" color = "background">
+            제목 테스트
+        </Title>
+        <Button size="large" scheme="primary">버튼 테스트</Button>
+        <InputText placeholder="여기에 입력하세요."/>
         </>
     );
 }
